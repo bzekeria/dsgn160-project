7b474a9e1497400d7dcbaeb68f9cdd6bcf38dfc4
Arina <147892821+arinakonnova@users.noreply.github.com>
2024-03-13 21:00:54 -0700

    Update 160 Project Code.R

--- 160 Project Code.R
+++ 160 Project Code.R
@@ -8,8 +8,10 @@ library(ggplot2)
 library(sampling)
 
 project_data <- read.csv("synthetic_experiment_data_group_yakovlev.csv")
+
 project_data$Group[project_data$Group=="control"] <- "0"
 project_data$Group[project_data$Group=="treatment 1"] <- "1"
 project_data$Group[project_data$Group=="treatment 2"] <- "2"
 project_data$Group[project_data$Group=="treatment 3"] <- "3"
+
 head(project_data)
