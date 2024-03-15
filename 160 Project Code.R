7466902374e09fbbb1c627eb35702a6ca2e23469
Arina <147892821+arinakonnova@users.noreply.github.com>
2024-03-14 19:55:56 -0700

    added satisfaction by treatment boxplot
    added satisfaction by treatment boxplot

--- 160 Project Code.R
+++ 160 Project Code.R
@@ -15,3 +15,10 @@ project_data$Group[project_data$Group=="treatment 2"] <- "2"
 project_data$Group[project_data$Group=="treatment 3"] <- "3"
 
 head(project_data)
+
+project_data_str <- project_data %>% mutate(Group = case_when
+    (Group == 0 ~ "control", Group == 1 ~ "5%", Group == 2 ~ "10%", Group == 3 ~ "15%"))
+satisfaction_bytreatment <- ggplot(project_data_str, aes(x = reorder(Group, satisfaction), y = satisfaction)) + 
+  geom_boxplot()
+satisfaction_bytreatment
+ggsave("satisfaction_boxplot.png", plot = satisfaction_bytreatment)
